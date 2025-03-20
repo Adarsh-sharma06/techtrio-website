58df929ca9def8f393c29ad49da0f001fa9f5408
Adarsh Sharma <adarshsharma06042001@gmail.com>
2025-03-21 02:38:50 +0530

    first commit 1

--- db/firebase.js
+++ db/firebase.js
@@ -1,15 +1,14 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvjC-2NpTSvokjToqRch5pScrhlJznQF4",
-  authDomain: "techtriowebsite.firebaseapp.com",
-  projectId: "techtriowebsite",
-  storageBucket: "techtriowebsite.firebasestorage.app",
-  messagingSenderId: "911023133346",
-  appId: "1:911023133346:web:b41a67fd978989a6f03dfb",
-  measurementId: "G-ZJF4QFR0TW",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
