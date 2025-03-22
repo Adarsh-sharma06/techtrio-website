19805d4fb047bf2bc08f6e1e5061bf2844a34688
Adarsh Sharma <adarshsharma06042001@gmail.com>
2025-03-22 18:02:51 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
+     <div>
+        <h2 className="text-4xl text-center font-bold text-gray-800">
+          Welcome to the Next.js Tailwind CSS Starter
+        </h2>
+        <p className="text-lg text-center text-gray-600 mt-4">
+          Get started by editing{" "}
+          <code className="bg-gray-100 p-1">src/app/page.js</code>
+        </p>
+      </div>
     </main>
   );
 }
